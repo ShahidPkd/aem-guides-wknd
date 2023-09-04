28d64c249108105d18b5102cfd76dd0cc9e3cbad
Shahid Siddiqui <shahidsiddiqui@rightpoint.com>
2023-09-04 16:41:44 +0530

    code for render conditions

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/TemplateRenderCondition.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/TemplateRenderCondition.java
@@ -1,4 +1,79 @@
 package com.adobe.aem.guides.wknd.core.models;
 
-public interface TemplateRenderCondition {
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+// import com.adobe.aem.guides.wknd.core.models.TemplateRenderCondition;
+import com.adobe.granite.ui.components.rendercondition.RenderCondition;
+import com.adobe.granite.ui.components.rendercondition.SimpleRenderCondition;
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.NameConstants;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Model(
+        adaptables = {SlingHttpServletRequest.class, Resource.class},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class TemplateRenderCondition{
+
+    // protected static final String RESOURCE_TYPE = "wknd/components/rendercondition/templates";
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected static final String REQUEST_PARAM_STRING = "item";
+
+    @Self
+    private SlingHttpServletRequest request;
+
+    @ValueMapValue
+    protected List<String> allowedTemplateTypes;
+
+    @ValueMapValue
+    protected String isComponent;
+
+    protected Boolean show = false;
+
+    @PostConstruct
+    protected void init() {
+        try {
+            String suffix = request.getRequestPathInfo().getSuffix();
+            String queryParam = request.getParameter(REQUEST_PARAM_STRING);
+
+            for (String allowedTemplateType : allowedTemplateTypes) {
+                if(StringUtils.isNotEmpty(suffix) && allowedTemplateType.equals(suffix)) {
+                    show = true;
+                } else if(StringUtils.isNotEmpty(queryParam)) {
+                    Resource pageResource = request.getResourceResolver().getResource(queryParam.concat("/").concat(JcrConstants.JCR_CONTENT));
+                    if(pageResource != null) {
+                        String templatePath = pageResource.getValueMap().get(NameConstants.NN_TEMPLATE, "");
+                        if (templatePath.equals(allowedTemplateType)) {
+                            show = true;
+                        }
+                    }
+                } else if(isComponent != null && isComponent.equals("true")) {
+                    Resource pageResource = request.getResourceResolver().getResource(suffix.substring(0, suffix.indexOf("/jcr:content")).concat("/").concat(JcrConstants.JCR_CONTENT));
+                    if(pageResource != null) {
+                        String templatePath = pageResource.getValueMap().get(NameConstants.NN_TEMPLATE, "");
+                        if (templatePath.equals(allowedTemplateType)) {
+                            show = true;
+                        }
+                    }
+                }
+            }
+            
+        } catch (IllegalArgumentException e) {
+            logger.error("Exception while procession the request: ", e);
+        }
+        request.setAttribute(RenderCondition.class.getName(), new SimpleRenderCondition(show));
+    }
 }
+
--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/TemplateRenderConditionImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/TemplateRenderConditionImpl.java
@@ -1,80 +1,80 @@
-package com.adobe.aem.guides.wknd.core.models.impl;
+// package com.adobe.aem.guides.wknd.core.models.impl;
 
-import java.util.List;
-import javax.annotation.PostConstruct;
+// import java.util.List;
+// import javax.annotation.PostConstruct;
 
-import com.adobe.aem.guides.wknd.core.models.TemplateRenderCondition;
-import com.adobe.granite.ui.components.rendercondition.RenderCondition;
-import com.adobe.granite.ui.components.rendercondition.SimpleRenderCondition;
-import com.day.cq.commons.jcr.JcrConstants;
-import com.day.cq.wcm.api.NameConstants;
+// import com.adobe.aem.guides.wknd.core.models.TemplateRenderCondition;
+// import com.adobe.granite.ui.components.rendercondition.RenderCondition;
+// import com.adobe.granite.ui.components.rendercondition.SimpleRenderCondition;
+// import com.day.cq.commons.jcr.JcrConstants;
+// import com.day.cq.wcm.api.NameConstants;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.apache.commons.lang3.StringUtils;
+// import org.apache.sling.api.SlingHttpServletRequest;
+// import org.apache.sling.api.resource.Resource;
+// import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+// import org.apache.sling.models.annotations.Model;
+// import org.apache.sling.models.annotations.injectorspecific.Self;
+// import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
-@Model(
-        adaptables = {SlingHttpServletRequest.class, Resource.class},
-        adapters = {TemplateRenderCondition.class},
-        resourceType = {TemplateRenderConditionImpl.RESOURCE_TYPE},
-        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
-)
-public class TemplateRenderConditionImpl implements TemplateRenderCondition {
+// @Model(
+//         adaptables = {SlingHttpServletRequest.class, Resource.class},
+//         adapters = {TemplateRenderCondition.class},
+//         resourceType = {TemplateRenderConditionImpl.RESOURCE_TYPE},
+//         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+// )
+// public class TemplateRenderConditionImpl implements TemplateRenderCondition {
 
-    protected static final String RESOURCE_TYPE = "wknd/components/rendercondition/templates";
+//     protected static final String RESOURCE_TYPE = "wknd/components/rendercondition/templates";
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+//     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    protected static final String REQUEST_PARAM_STRING = "item";
+//     protected static final String REQUEST_PARAM_STRING = "item";
 
-    @Self
-    private SlingHttpServletRequest request;
+//     @Self
+//     private SlingHttpServletRequest request;
 
-    @ValueMapValue
-    protected List<String> allowedTemplateTypes;
+//     @ValueMapValue
+//     protected List<String> allowedTemplateTypes;
 
-    @ValueMapValue
-    protected String isComponent;
+//     @ValueMapValue
+//     protected String isComponent;
 
-    protected Boolean show = false;
+//     protected Boolean show = false;
 
-    @PostConstruct
-    protected void init() {
-        try {
-            String suffix = request.getRequestPathInfo().getSuffix();
-            String queryParam = request.getParameter(REQUEST_PARAM_STRING);
+//     @PostConstruct
+//     protected void init() {
+//         try {
+//             String suffix = request.getRequestPathInfo().getSuffix();
+//             String queryParam = request.getParameter(REQUEST_PARAM_STRING);
 
-            for (String allowedTemplateType : allowedTemplateTypes) {
-                if(StringUtils.isNotEmpty(suffix) && allowedTemplateType.equals(suffix)) {
-                    show = true;
-                } else if(StringUtils.isNotEmpty(queryParam)) {
-                    Resource pageResource = request.getResourceResolver().getResource(queryParam.concat("/").concat(JcrConstants.JCR_CONTENT));
-                    if(pageResource != null) {
-                        String templatePath = pageResource.getValueMap().get(NameConstants.NN_TEMPLATE, "");
-                        if (templatePath.equals(allowedTemplateType)) {
-                            show = true;
-                        }
-                    }
-                } else if(isComponent != null && isComponent.equals("true")) {
-                    Resource pageResource = request.getResourceResolver().getResource(suffix.substring(0, suffix.indexOf("/jcr:content")).concat("/").concat(JcrConstants.JCR_CONTENT));
-                    if(pageResource != null) {
-                        String templatePath = pageResource.getValueMap().get(NameConstants.NN_TEMPLATE, "");
-                        if (templatePath.equals(allowedTemplateType)) {
-                            show = true;
-                        }
-                    }
-                }
-            }
+//             for (String allowedTemplateType : allowedTemplateTypes) {
+//                 if(StringUtils.isNotEmpty(suffix) && allowedTemplateType.equals(suffix)) {
+//                     show = true;
+//                 } else if(StringUtils.isNotEmpty(queryParam)) {
+//                     Resource pageResource = request.getResourceResolver().getResource(queryParam.concat("/").concat(JcrConstants.JCR_CONTENT));
+//                     if(pageResource != null) {
+//                         String templatePath = pageResource.getValueMap().get(NameConstants.NN_TEMPLATE, "");
+//                         if (templatePath.equals(allowedTemplateType)) {
+//                             show = true;
+//                         }
+//                     }
+//                 } else if(isComponent != null && isComponent.equals("true")) {
+//                     Resource pageResource = request.getResourceResolver().getResource(suffix.substring(0, suffix.indexOf("/jcr:content")).concat("/").concat(JcrConstants.JCR_CONTENT));
+//                     if(pageResource != null) {
+//                         String templatePath = pageResource.getValueMap().get(NameConstants.NN_TEMPLATE, "");
+//                         if (templatePath.equals(allowedTemplateType)) {
+//                             show = true;
+//                         }
+//                     }
+//                 }
+//             }
             
-        } catch (IllegalArgumentException e) {
-            logger.error("Exception while procession the request: ", e);
-        }
-        request.setAttribute(RenderCondition.class.getName(), new SimpleRenderCondition(show));
-    }
-}
+//         } catch (IllegalArgumentException e) {
+//             logger.error("Exception while procession the request: ", e);
+//         }
+//         request.setAttribute(RenderCondition.class.getName(), new SimpleRenderCondition(show));
+//     }
+// }
